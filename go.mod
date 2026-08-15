54bb32bdb6f743d5627c38bd2c52c54092a60328
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2026-08-15 16:14:47 +0500

    feat: add migrations

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module AvitoWinter25
 
 go 1.25
+
+require github.com/google/uuid v1.6.0
--- migrations/0001_init.up.sql
+++ migrations/0001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id              uuid PRIMARY KEY,
+    id              uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     username        character varying(255) UNIQUE NOT NULL,
     hashed_password character varying(60)         NOT NULL,
     coins           integer                       NOT NULL default 1000 CHECK ( coins >= 0 )
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS transaction
 (
-    id        uuid PRIMARY KEY,
+    id        uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     from_user uuid REFERENCES users (id),
     to_user   uuid REFERENCES users (id),
     amount    integer not null CHECK ( amount > 0 )
@@ -20,7 +20,7 @@ CREATE INDEX idx_to ON transaction (to_user);
 
 CREATE TABLE merch
 (
-    id    uuid PRIMARY KEY,
+    id    uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name  character varying(31) UNIQUE NOT NULL,
     price integer                      NOT NULL CHECK ( price > 0 )
 );
