d6d3b9d0e54dab8b8a54f49e3dd8d834b9912130
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2026-08-20 00:47:49 +0500

    feat: add coins

--- internal/repo/coin.go
+++ internal/repo/coin.go
@@ -1,19 +1,44 @@
 package repo
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type CoinRepo struct {
-	conn pgx.Conn
+	pool *pgxpool.Pool
 	log  *slog.Logger
 }
 
-func NewCoinRepo(conn pgx.Conn, log *slog.Logger) *CoinRepo {
+func NewCoinRepo(pool *pgxpool.Pool, log *slog.Logger) *CoinRepo {
 	return &CoinRepo{
-		conn: conn,
+		pool: pool,
 		log:  log,
 	}
+}
+
+func (r *CoinRepo) AddCoins(ctx context.Context, username string, amount int) error {
+	query := `UPDATE users SET coins = coins + $1 WHERE username = $2`
+
+	_, err := r.pool.Exec(ctx, query, amount, username)
+	return err
+}
+
+func (r *CoinRepo) GetCoins(ctx context.Context, username string) (int, error) {
+	var coins int
+
+	query := `SELECT coins from users WHERE username = $1`
+	err := r.pool.QueryRow(ctx, query, username).Scan(&coins)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, nil
+		}
+		return 0, err
+	}
+
+	return coins, nil
 }
--- internal/service/coin/coin.go
+++ internal/service/coin/coin.go
@@ -1,1 +1,35 @@
 package coin
+
+import (
+	"context"
+	"log/slog"
+)
+
+type Repo interface {
+	AddCoins(ctx context.Context, username string, amount int) error
+	GetCoins(ctx context.Context, username string) (int, error)
+}
+
+type Service struct {
+	log  *slog.Logger
+	repo Repo
+}
+
+func New(log *slog.Logger, repo Repo) *Service {
+	return &Service{
+		log:  log,
+		repo: repo,
+	}
+}
+
+func (s *Service) GetCoins(ctx context.Context, username string) (int, error) {
+	panic("implement me")
+}
+
+func (s *Service) BuyMerch(ctx context.Context, username, merch string) error {
+	panic("implement me")
+}
+
+func (s *Service) Transact(ctx context.Context, userFrom string, userTo string, amount int) error {
+	panic("implement me")
+}
