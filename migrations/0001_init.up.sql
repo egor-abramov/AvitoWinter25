8a25efca0be0eb7a481540bb5886723668e13c28
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2026-08-21 01:08:00 +0500

    fix: add id column to user_merch

--- migrations/0001_init.up.sql
+++ migrations/0001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id              uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id              uuid PRIMARY KEY                       DEFAULT gen_random_uuid(),
     username        character varying(255) UNIQUE NOT NULL,
     hashed_password character varying(60)         NOT NULL,
     coins           integer                       NOT NULL default 1000 CHECK ( coins >= 0 )
@@ -27,8 +27,7 @@ CREATE TABLE merch
 
 CREATE TABLE IF NOT EXISTS user_merch
 (
+    id       uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id  uuid REFERENCES users (id),
-    merch_id uuid REFERENCES merch (id),
-    quantity integer NOT NULL CHECK ( quantity >= 0 ),
-    PRIMARY KEY (user_id, merch_id)
+    merch_id uuid REFERENCES merch (id)
 );
