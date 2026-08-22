a1e225ce38bfe5d931e1d696b4ea504a179f9341
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2026-08-22 22:24:17 +0500

    feat: configure DI

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,10 +7,16 @@ import (
 	"AvitoWinter25/internal/infrastructure/postgres"
 	"AvitoWinter25/internal/repo"
 	"AvitoWinter25/internal/service/auth"
+	"AvitoWinter25/internal/service/coin"
+	"AvitoWinter25/internal/service/info"
+	"AvitoWinter25/internal/service/merch"
 	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 
+	trmpgx "github.com/avito-tech/go-transaction-manager/drivers/pgxv5/v2"
+	"github.com/avito-tech/go-transaction-manager/trm/v2/manager"
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -27,14 +33,33 @@ func New(log *slog.Logger, cfg *config.Config) (*App, error) {
 		return nil, err
 	}
 
-	userRepo := repo.NewUserRepo(pool, nil)
+	userRepo := repo.NewUserRepo(pool)
+	coinRepo := repo.NewCoinRepo(pool)
+	merchRepo := repo.NewMerchRepo(pool)
+
+	trFactory := trmpgx.NewFactory(pool)
+	txManager, err := manager.New(trFactory)
+	if err != nil {
+		return nil, err
+	}
+
 	authService := auth.New(userRepo, log, cfg.JWT.Secret, cfg.JWT.ExpTime)
+	coinService := coin.New(coinRepo, txManager)
+	merchService := merch.New(merchRepo, coinRepo, userRepo, txManager)
+	infoService := info.New(coinService, merchService)
+
 	authHandler := handler.NewAuthHandler(authService, log)
+	coinHandler := handler.NewSendCoinHandler(coinService, log)
+	merchHandler := handler.NewBuyHandler(merchService, log)
+	infoHandler := handler.NewInfoHandler(infoService, log)
 
 	r := chi.NewRouter()
 	r.Post("/api/auth", authHandler)
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.NewJWTExtractor(cfg.JWT.Secret))
+		r.Get("/api/info", infoHandler)
+		r.Post("/api/sendCoin", coinHandler)
+		r.Get("/api/buy/{item}", merchHandler)
 	})
 
 	srv := &http.Server{
@@ -53,7 +78,7 @@ func (a *App) Run() {
 	a.log.Info("starting http server", "address", a.server.Addr)
 
 	go func() {
-		if err := a.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := a.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			a.log.Error("failed to start server", "error", err)
 		}
 	}()
